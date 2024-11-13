8e6d9b848274dfd91d5698a79140b837647d963c
Garibaldi Adriano Neto <Garibaldi_Adriano@live.com>
2024-11-13 16:58:07 -0300

    Inclusão do Prisma

---
+++ src/models/marcaModel.ts
@@ -0,0 +1,11 @@
+// import { UUID } from "crypto"
+
+// interface Marca {
+//     id : number
+//     nome : string,
+//     modelos: modelo[]
+// }
+
+// let marca : Marca [] = []
+
+// export {Marca, marca}
