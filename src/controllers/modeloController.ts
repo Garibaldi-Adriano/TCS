9c168d7fd55f408469d8a699e421dbd53b1ca196
Garibaldi Adriano Neto <Garibaldi_Adriano@live.com>
2024-11-14 17:14:46 -0300

    atualizacao rotas e controllers

---
+++ src/controllers/modeloController.ts
@@ -0,0 +1,150 @@
+import { Request, Response } from 'express';
+import { PrismaClient, Prisma } from '@prisma/client';
+import { ErrorRequest } from '../utils/TratamentoErros';
+
+const prisma = new PrismaClient();
+
+// Listar todos os modelos
+export const getAllModelos = async (req: Request, res: Response) => {
+    try {
+        const modelos = await prisma.modelo.findMany({
+            include: {
+                veiculos: true,
+                marca: true,
+                categoria: true
+            }
+        });
+        res.status(200).json(modelos);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.log(error.code);
+            res.status(409).json({
+                error: {
+                    message: ErrorRequest.errorRequest(error.code),
+                    field: error.meta
+                }
+            });
+        }
+    }
+};
+
+// Obter um modelo pelo ID
+export const getModeloById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const modelo = await prisma.modelo.findUnique({
+            where: { id: Number(id) },
+            include: {
+                veiculos: true,
+                marca: true,
+                categoria: true
+            }
+        });
+
+        if (!modelo) {
+            return res.status(404).json({ error: 'Modelo não encontrado' });
+        }
+
+        res.status(200).json(modelo);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.log(error.code);
+            res.status(409).json({
+                error: {
+                    message: ErrorRequest.errorRequest(error.code),
+                    field: error.meta
+                }
+            });
+        }
+    }
+};
+
+// Criar um novo modelo
+export const createModelo = async (req: Request, res: Response) => {
+    const { nome, anoModelo, qtModelo, marcaId, categoriaId } = req.body;
+
+    if (!nome || !anoModelo || !qtModelo || !marcaId || !categoriaId) {
+        res.status(401).json({message: "Parametros invalidos. Necessário informar: nome, anoModelo, qtModelo, marcaId, categoriaId."})
+    }
+
+    try {
+        const modelo = await prisma.modelo.create({
+            data: {
+                nome,
+                anoModelo: new Date(anoModelo),
+                qtModelo,
+                marcaId,
+                categoriaId
+            }
+        });
+        res.status(201).json(modelo);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.log(error.code);
+            res.status(409).json({
+                error: {
+                    message: ErrorRequest.errorRequest(error.code),
+                    field: error.meta
+                }
+            });
+        }
+    }
+};
+
+// Atualizar um modelo
+export const updateModelo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { nome, anoModelo, qtModelo, marcaId, categoriaId } = req.body;
+
+    if (!nome || !anoModelo || !qtModelo || !marcaId || !categoriaId) {
+        res.status(401).json({message: "Parametros invalidos. Necessário informar: nome, anoModelo, qtModelo, marcaId, categoriaId."})
+    }
+
+    try {
+        const modelo = await prisma.modelo.update({
+            where: { id: Number(id) },
+            data: {
+                nome,
+                anoModelo: new Date(anoModelo),
+                qtModelo,
+                marcaId,
+                categoriaId
+            }
+        });
+
+        res.status(200).json(modelo);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.log(error.code);
+            res.status(409).json({
+                error: {
+                    message: ErrorRequest.errorRequest(error.code),
+                    field: error.meta
+                }
+            });
+        }
+    }
+};
+
+// Deletar um modelo
+export const deleteModelo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        await prisma.modelo.delete({
+            where: { id: Number(id) }
+        });
+        res.status(200).json({ message: 'Modelo deletado com sucesso' });
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.log(error.code);
+            res.status(409).json({
+                error: {
+                    message: ErrorRequest.errorRequest(error.code),
+                    field: error.meta
+                }
+            });
+        }
+    }
+};
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,70 @@
 import { Router } from 'express';
-// import {create, researchAll,update,deleted } from '../controllers/clienteController'
+import { getAllMarcas, getMarcaById, createMarca, updateMarca, deleteMarca } from '../controllers/marcaController';
+import { getAllModelos, getModeloById, createModelo, updateModelo, deleteModelo } from '../controllers/modeloController';
+import { getAllCategorias, getCategoriaById, createCategoria, updateCategoria, deleteCategoria } from '../controllers/categoriaController';
+// import { getAllVeiculos, getVeiculoById, createVeiculo, updateVeiculo, deleteVeiculo } from '../controllers/veiculoController';
+// import { getAllManutencoes, getManutencaoById, createManutencao, updateManutencao, deleteManutencao } from '../controllers/manutencaoController';
+// import { getAllOcorrencias, getOcorrenciaById, createOcorrencia, updateOcorrencia, deleteOcorrencia } from '../controllers/ocorrenciaController';
+// import { getAllContratoLocacoes, getContratoLocacaoById, createContratoLocacao, updateContratoLocacao, deleteContratoLocacao } from '../controllers/contratoLocacaoController';
+// import { getAllPagamentos, getPagamentoById, createPagamento, updatePagamento, deletePagamento } from '../controllers/pagamentoController';
 
 const router = Router()
 
-// router.post('/cliente',create)
-// router.get('/cliente',researchAll)
-// router.put('/cliente/:id',update)
-// router.delete('/cliente/:id',deleted)
+
+//***** MARCA ******** */
+router.post('/marcas', createMarca)
+router.get('/marcas', getAllMarcas)
+// router.get('/marcas/:id', getMarcaById)
+// router.put('/marcas/:id',updateMarca)
+router.delete('/marcas/:id', deleteMarca)
+
+//***** MODELO ******** */
+router.post('/modelos', createModelo)
+router.get('/modelos', getAllModelos)
+// router.get('/modelos/:id', getModeloById)
+// router.put('/modelos/:id',updateModelo)
+router.delete('/modelos/:id', deleteModelo)
+
+//***** CATEGORIA ******** */
+router.post('/categorias', createCategoria)
+router.get('/categorias', getAllCategorias)
+// router.get('/categorias/:id', getCategoriaById)
+router.put('/categorias/:id',updateCategoria)
+router.delete('/categorias/:id', deleteCategoria)
+
+//***** VEICULO ******** */
+// router.post('/veiculos', createVeiculo)
+// router.get('/veiculos', getAllVeiculos)
+// router.get('/veiculos/:id', getVeiculoById)
+// router.put('/veiculos/:id',updateVeiculo)
+// router.delete('/veiculos/:id', deleteVeiculo)
+
+//***** MANUTENCAO ******** */
+// router.post('/manutencoes', createManutencao)
+// router.get('/manutencoes', getAllManutencoes)
+// router.get('/manutencoes/:id', getManutencaoById)
+// router.put('/manutencoes/:id',updateManutencao)
+// router.delete('/manutencoes/:id', deleteManutencao)
+
+//***** OCORRENCIA ******** */
+// router.post('/ocorrencias', createOcorrencia)
+// router.get('/ocorrencias', getAllOcorrencias)
+// router.get('/ocorrencias/:id', getOcorrenciaById)
+// router.put('/ocorrencias/:id',updateOcorrencia)
+// router.delete('/ocorrencias/:id', deleteOcorrencia)
+
+//***** CONTRATO LOCACAO ******** */
+// router.post('/contratoLocacoes', createContratoLocacao)
+// router.get('/contratoLocacoes', getAllContratoLocacoes)
+// router.get('/contratoLocacoes/:id', getContratoLocacaoById)
+// router.put('/contratoLocacoes/:id',updateContratoLocacao)
+// router.delete('/contratoLocacoes/:id', deleteContratoLocacao)
+
+//***** PAGAMENTO ******** */
+// router.post('/pagamentos', createPagamento)
+// router.get('/pagamentos', getAllPagamentos)
+// router.get('/pagamentos/:id', getPagamentoById)
+// router.put('/pagamentos/:id',updatePagamento)
+// router.delete('/pagamentos/:id', deletePagamento)
 
 export default router
